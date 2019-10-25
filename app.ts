1361433edd8db86913cd2555829cadff9ce870c1
stella-celentano <stellacelentano99@gmail.com>
2019-10-25 20:35:25 -0300

    Configurações e Primeiro Exemplo

---
+++ app.ts
@@ -0,0 +1,15 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+//episode = '4' vai aparecer na tela que o proximo ep é o 41, pois ele executa mesmo sendo variáveis incompativeis
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+
+//let favoriteDroid declara a variavel como tipo ANY (aceita qualquer tipo de variável)
+let favoriteDroid: string
+favoriteDroid = 'BB-8'
+//favoriteDroid = 10
+console.log("My favorite droid is " + favoriteDroid)
---
+++ dist/app.js
@@ -0,0 +1,12 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+//episode = '4' vai aparecer na tela que o proximo ep é o 41, pois ele executa mesmo sendo variáveis incompativeis
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+//let favoriteDroid declara a variavel como tipo ANY (aceita qualquer tipo de variável)
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+//favoriteDroid = 10
+console.log("My favorite droid is " + favoriteDroid);
