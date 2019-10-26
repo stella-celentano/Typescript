ccc18e020b6d43961d5a64005d3a6d2390734048
stella-celentano <stellacelentano99@gmail.com>
2019-10-25 21:03:11 -0300

    Usando Funções

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+//PRIMEIRO EXEMPLO
 let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
 console.log(message)
 
@@ -7,9 +8,26 @@ console.log("This is episode " + 4)
 episode = episode + 1
 console.log("Next episode is " + episode)
 
-
 //let favoriteDroid declara a variavel como tipo ANY (aceita qualquer tipo de variável)
 let favoriteDroid: string
 favoriteDroid = 'BB-8'
 //favoriteDroid = 10
 console.log("My favorite droid is " + favoriteDroid)
+
+
+//USANDO FUNÇÕES
+let isEnoughToBeatMF = function (parsecs: number) : boolean{
+    return parsecs < 12
+}
+
+let distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnoughToBeatMF(distance) ? 'YES': 'NO'}`)
+
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+function inc (speed: number, inc: number = 1) : number {
+    return speed + inc
+}
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,4 @@
+//PRIMEIRO EXEMPLO
 var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
 console.log(message);
 var episode = 4;
@@ -10,3 +11,17 @@ var favoriteDroid;
 favoriteDroid = 'BB-8';
 //favoriteDroid = 10
 console.log("My favorite droid is " + favoriteDroid);
+//USANDO FUNÇÕES
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
