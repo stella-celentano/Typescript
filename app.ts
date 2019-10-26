b61877fea47890f708da6e92b73367096127d96f
stella-celentano <stellacelentano99@gmail.com>
2019-10-26 18:54:37 -0300

    Usando Classes e Interfaces

--- app.ts
+++ app.ts
@@ -31,3 +31,47 @@ function inc (speed: number, inc: number = 1) : number {
 }
 console.log(`inc (5,1) = ${inc(5,1)}`)
 console.log(`inc (5) = ${inc(5)}`)
+
+
+//CLASSES E INTERFACES
+class Spacecraft {
+
+    constructor (public propulsor: string){}
+
+    jumpIntoHyperspace() {
+        console.log(`Entering hyperspace with ${this.propulsor}`) //precisa usar o this por estar utilizando um membro da instancia dentro da classe
+    }
+}
+
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
+
+class MillenniumFalcon extends Spacecraft implements Containership{
+
+    cargoContainers: number
+
+    constructor(){
+        super('hyperdrive')
+        this.cargoContainers = 2
+    }
+
+    jumpIntoHyperspace(){
+        if (Math.random() >= 0.5){
+            super.jumpIntoHyperspace()
+        }else {
+            console.log('Failed to jump into hyperspace')
+        }
+    }
+}
+
+let falcon = new MillenniumFalcon()
+falcon.jumpIntoHyperspace()
+
+interface Containership { //n tem uma estrutura que represente em js
+
+    cargoContainers: number //colocando uma ? na frente do atributo ou metodo ele se torna opcional
+}
+
+let goodForTheJob = ( ship: Containership ) => ship.cargoContainers > 2
+
+console.log(`Is falcon good for the job? ${goodForTheJob ( falcon ) ? 'yes': 'no'}`)
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //PRIMEIRO EXEMPLO
 var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
 console.log(message);
@@ -25,3 +38,36 @@ function inc(speed, inc) {
 }
 console.log("inc (5,1) = " + inc(5, 1));
 console.log("inc (5) = " + inc(5));
+//CLASSES E INTERFACES
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(propulsor) {
+        this.propulsor = propulsor;
+    }
+    Spacecraft.prototype.jumpIntoHyperspace = function () {
+        console.log("Entering hyperspace with " + this.propulsor); //precisa usar o this por estar utilizando um membro da instancia dentro da classe
+    };
+    return Spacecraft;
+}());
+var ship = new Spacecraft('hyperdrive');
+ship.jumpIntoHyperspace();
+var MillenniumFalcon = /** @class */ (function (_super) {
+    __extends(MillenniumFalcon, _super);
+    function MillenniumFalcon() {
+        var _this = _super.call(this, 'hyperdrive') || this;
+        _this.cargoContainers = 2;
+        return _this;
+    }
+    MillenniumFalcon.prototype.jumpIntoHyperspace = function () {
+        if (Math.random() >= 0.5) {
+            _super.prototype.jumpIntoHyperspace.call(this);
+        }
+        else {
+            console.log('Failed to jump into hyperspace');
+        }
+    };
+    return MillenniumFalcon;
+}(Spacecraft));
+var falcon = new MillenniumFalcon();
+falcon.jumpIntoHyperspace();
+var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
+console.log("Is falcon good for the job? " + (goodForTheJob(falcon) ? 'yes' : 'no'));
